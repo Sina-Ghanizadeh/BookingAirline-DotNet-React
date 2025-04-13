9761b564ada7f35b2906b45dd4ec6fa96716f036
Sina Ghanizadeh <Ghanizadeh@radshid.com>
2025-04-13 16:26:01 +0330

    feat: define domain models

---
+++ BookingAirline.Domain/Models/Flight.cs
@@ -0,0 +1,53 @@
+﻿namespace BookingAirline.Domain.Models;
+
+public class Flight
+{
+    public string FlightNumber { get; set; }
+    public string DepartureAirport { get; set; }
+    public string DestinationAirport { get; set; }
+    public DateTime DepartureTime { get; set; }
+    public DateTime ArrivalTime { get; set; }
+    public int Capacity { get; set; }
+    public int AvailableSeats { get; set; }
+    public decimal TicketPrice { get; set; }
+    public List<Ticket> Tickets { get; set; }
+
+    public Flight(string flightNumber, string departureAirport, string destinationAirport, DateTime departureTime, DateTime arrivalTime, int capacity, decimal ticketPrice)
+    {
+        FlightNumber = flightNumber;
+        DepartureAirport = departureAirport;
+        DestinationAirport = destinationAirport;
+        DepartureTime = departureTime;
+        ArrivalTime = arrivalTime;
+        Capacity = capacity;
+        AvailableSeats = capacity;
+        TicketPrice = ticketPrice;
+        Tickets = new List<Ticket>();
+    }
+
+    public void GetFlightDetails()
+    {
+        Console.WriteLine($"Flight Number: {FlightNumber}");
+        Console.WriteLine($"From: {DestinationAirport} To: {DestinationAirport}");
+        Console.WriteLine($"Departure Time: {DepartureTime}, Arrival Time: {ArrivalTime}");
+        Console.WriteLine($"Available Seats: {AvailableSeats}/{Capacity}");
+        Console.WriteLine($"Ticket Price: {TicketPrice:C}");
+    }
+    public int CheckAvailableSeats()
+    {
+        return AvailableSeats;
+    }
+    public void BookTicket(Ticket ticket)
+    {
+        if (AvailableSeats > 0)
+        {
+            Tickets.Add(ticket);
+            AvailableSeats--;
+            ticket.Passenger.Tickets.Add(ticket);
+        }
+        else
+        {
+            Console.WriteLine("No available seats.");
+        }
+    }
+}
---
+++ BookingAirline.Domain/Models/Passenger.cs
@@ -0,0 +1,25 @@
+﻿namespace BookingAirline.Domain.Models;
+
+public class Passenger
+{
+    public Guid PassengerId { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+    public List<Ticket> Tickets { get; set; }
+    public Passenger(string name, string email,string phoneNumber)
+    {
+        PassengerId = Guid.NewGuid();
+        Name = name;
+        Email = email;
+        PhoneNumber = phoneNumber;
+        Tickets = new List<Ticket>();
+    }
+    public void GetPassengerDetails()
+    {
+        Console.WriteLine($"Passenger ID: {PassengerId}");
+        Console.WriteLine($"Name: {Name}");
+        Console.WriteLine($"Email: {Email}");
+        Console.WriteLine($"Phone Number: {PhoneNumber}");
+    }
+}
