056cffc33627a1c77307fdd0cec945a967383cf0
Sina Ghanizadeh <Ghanizadeh@radshid.com>
2025-04-13 17:00:39 +0330

    feat: define infrastructure

---
+++ BookingAirline.Infrastructure/DependencyContainer.cs
@@ -0,0 +1,13 @@
+﻿using BookingAirline.Application.Interfaces;
+using BookingAirline.Application.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BookingAirline.Infrastructure;
+
+public static class DependencyContainer
+{
+    public static void RegisterServices(this IServiceCollection services)
+    {
+        services.AddScoped<IBookingService, BookingService>();
+    }
+}
